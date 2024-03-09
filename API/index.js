117fa0657d8cee0a546c9682f2b75d7ba8b59f91
LucasSan1 <137550683+LucasSan1@users.noreply.github.com>
2024-03-09 20:35:19 -0300

    Add files via upload

--- API/index.js
+++ API/index.js
@@ -1,14 +1,22 @@
 const cors =  require('cors');
 const express = require('express');
 const app = express();
-const routes = require('./routes/routes');
 const port = process.env.PORT || 3000;
 
 //Midlewares da API
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/', routes)
+
+
+//Rotas
+const rotas_usuario = require('./routes/usuarios');
+
+app.use("", rotas_usuario); 
+
+app.use("/", (req, res) => {
+    res.json("Ta rodando a API, está é a /home");
+  });
 
 app.listen(port, () => {
     console.log(`Conectado na porta: ${port}`)
