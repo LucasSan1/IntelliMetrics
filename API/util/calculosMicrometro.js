27f031d310e554b5971c8ab8c718df2c181327c7
LucasSan1 <137550683+LucasSan1@users.noreply.github.com>
2024-03-28 14:41:28 -0300

    Add files via upload

--- API/util/calculosMicrometro.js
+++ API/util/calculosMicrometro.js
@@ -1,37 +1,49 @@
 function calculoPlaneza(CMovel, CFixo) {
-  const mediaCMovel = media(CMovel).toFixed(0);
-  const mediaCFixo = media(CFixo).toFixed(0);
-  let varianciaCfixo = 0;
-  let varianciaCmovel = 0;
 
-  //Calculo desvio padrao CMovel
-  for (let i = 0; i < CMovel.length; i++) {
-    varianciaCmovel += Math.pow(mediaCMovel - CMovel[i], 2);
-  }
+  const NCmovel = CMovel.length;
+  const NCFixo = CFixo.length;
 
-  varianciaCmovel = varianciaCmovel / CMovel.length;
+  // Desvio padrao do CMovel
+  // Calcula a média dos valores
+  const mediaCM = CMovel.reduce((acc, val) => acc + val, 0) / NCmovel;
+  const mediaCMTratada = mediaCM.toFixed(0)
 
-  const desvPadCMovel = Math.sqrt(varianciaCmovel) * 0.0003;
-  const desvPadMovelArredondado = desvPadCMovel.toFixed(4);
+  // Calcula a soma dos quadrados das diferenças entre cada valor e a média
+  const somaQuadradosDiffCM = CMovel.reduce((acc, val) => acc + Math.pow(val - mediaCM, 2), 0);
 
-  //Calculo desvio padrao Cfixo
-  for (let i = 0; i < CFixo.length; i++) {
-    varianciaCfixo += Math.pow(mediaCFixo - CFixo[i], 2);
-  }
+  // Calcula o desvio padrãzo da amostra
+  const desvioPadraoCM = Math.sqrt(somaQuadradosDiffCM / (NCmovel - 1)) * 0.0003;
+  const desvioPadraoTratadoCM = desvioPadraoCM.toFixed(4)
+  
 
-  varianciaCfixo = varianciaCfixo / CFixo.length;
-  const desvPadCFixo = Math.sqrt(varianciaCfixo) * 0.0003;
-  const desvPadFixoArredondado = desvPadCFixo.toFixed(4);
+  //Desvio padrao do CFixo
+  // Calcula a média dos valores
+  const mediaCF = CFixo.reduce((acc, val) => acc + val, 0) / NCFixo;
+  const mediaCFTratada = mediaCF.toFixed(0)
 
-  console.log("CMovel", desvPadMovelArredondado);
-  console.log("CFixo", desvPadFixoArredondado);
+  // Calcula a soma dos quadrados das diferenças entre cada valor e a média
+  const somaQuadradosDiffCF = CFixo.reduce((acc, val) => acc + Math.pow(val - mediaCF, 2), 0);
+
+  // Calcula o desvio padrão da amostra
+  const desvioPadraoCF = Math.sqrt(somaQuadradosDiffCF / (NCFixo - 1)) * 0.0003;
+  const desvioPadraoTratadoCF = desvioPadraoCF.toFixed(4)
+
+  // Calcula a planeza média das amostras
+  const planezaMedia = ((mediaCM + mediaCF) / 2)*0.0003
+  const planezaMediaTratada = planezaMedia.toFixed(4)
+
+  const resultados = {
+    media_CMovel: mediaCMTratada,
+    media_CFixo: mediaCFTratada,
+    desvioPadraoCMovel: desvioPadraoTratadoCM,
+    desvioPadraoCFixo: desvioPadraoTratadoCF,
+    planezaMedia: planezaMediaTratada
+  };
+
+  return resultados;
+
 }
 
-function media(a) {
-  let soma = 0;
-  a.map((numero) => (soma += numero));
-  return soma / a.length;
-}
 
 module.exports = {
   calculoPlaneza,
