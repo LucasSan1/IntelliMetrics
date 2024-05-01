ade14abd27828defa604c5a799e6c9b48e736c7b
lucas <evangelistalucas034@gmail.com>
2024-05-01 13:54:01 -0300

    feat and Fix - rotas de usuarios

--- API/routes/usuarios.js
+++ API/routes/usuarios.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const validacaoUsuario = require("../validation/usuariosVal");
 
 // const { generateToken, verifica, removerToken } = require("../controller/token");
-const { createUser, getCol, getColById, deleteCol, putCol, login } = require("../controllers/controllerUser");
+const { createUser, getCol, getColById, disableUser, enableUser, putPass, putUser, login } = require("../controllers/controllerUser");
 const { middlewareValidarJWT } = require("../middleware/authMiddleware");
 
 router
@@ -45,21 +45,19 @@ router
     }
   })
   
-  .delete("/collaborator/:id", middlewareValidarJWT, async (req, res) => {
+  .put("/disableUser", middlewareValidarJWT, async (req, res) => {
     try {
-      const id = req.params.id;
-      const nome = req.body.nome;
+        const {email} = req.body
 
-      let resultado = await deleteCol(
-        id,
-        nome.toLowerCase()
+      let resultado = await disableUser(
+        email.toLowerCase()
       );
       switch (resultado) {
         case 200:
-          res.status(200).json('Usuário deletado')
+          res.status(200).json('Usuário desativado')
           break;
         case 400:
-          res.status(400).json('Usuário já deletado')
+          res.status(400).json('Usuário já esta desativado')
           break;
         default:
           res.status(500).json('Erro interno do servidor')
@@ -69,7 +67,31 @@ router
       console.log(error)
     }
   })
-  .get("/collaborator", middlewareValidarJWT, async (req, res) => {
+
+  .put("/enableUser", middlewareValidarJWT, async (req, res) => {
+    try {
+        const {email} = req.body
+
+      let resultado = await enableUser(
+        email.toLowerCase()
+      );
+      switch (resultado) {
+        case 200:
+          res.status(200).json('Usuário ativado')
+          break;
+        case 400:
+          res.status(400).json('Usuário já esta ativo')
+          break;
+        default:
+          res.status(500).json('Erro interno do servidor')
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+  .get("/allUsers", middlewareValidarJWT, async (req, res) => {
     try {
       const email = req.body.email;
 
@@ -83,7 +105,7 @@ router
     }
   })
 
-  .get("/collaborator/:id", middlewareValidarJWT, async (req, res) => {
+  .get("/user/:id", middlewareValidarJWT, async (req, res) => {
     try {
       const id = req.params.id;
 
@@ -97,53 +119,59 @@ router
       console.log(error)
     }
   })
-  // atualizar usuario
-  .put("/collaboratorUP/:id", middlewareValidarJWT, async (req, res) => {
+
+  // atualizar senha usuario
+  .put("/updatePass", middlewareValidarJWT, async (req, res) => {
     try {
-      const id = req.params.id;
-      const { email, senhaNova, nome } = req.body;
+      const { email, senhaNova } = req.body;
 
-
-
-      let resultado = await putCol(
-        id,
+      let resultado = await putPass(
         email.toLowerCase(),
-        senhaNova,
-        nome.toLowerCase()
+        senhaNova
       );
-      if (resultado === 400) {
-        res.status(200).json("Cliente Não Encontrado")
-      } else if (resultado) {
-        res.status(200).json("Cliente atualizado")
-      } else {
-        res.status(500).json("Erro interno do servidor")
-      }
 
+      switch(resultado){
+        case 404:
+          res.status(404).json("Cliente Não Encontrado")
+          break
+        case 200:
+            res.status(200).json("Cliente atualizado")
+            break;
+        case 400:
+            res.status(400).json("Não foi possivel redefinir senha")
+            break;
+        default:
+            res.status(500).json("Erro interno do servidor")
+            break;
+      }
+ 
     } catch (error) {
       console.log(error)
     }
 
   })
   // atualizar token
-  .put("/collaborator/login", async (req, res) => {
+  .put("/user/login", async (req, res) => {
     try {
 
       const { email, senha } = req.body;
       let resultado = await login(
-
         email.toLowerCase(),
-        senha.toLowerCase()
-
+        senha
       );
 
-
-      if (resultado === 401) {
-        res.status(400).json("Preencha todos os campos")
+      switch(resultado){
+        case 400:
+            res.status(400).json("Preencha todos os campos")
+            break;
+        case 401:
+            res.status(401).json("Credenciais inválidas")
+            break;
+        case 404:
+            res.status(404).json("Usuário não encontrado")
+            break;
       }
-
-      if (resultado === 400) {
-        res.status(400).json("Credenciais inválidas")
-      }
+     
       const dadosUsuario = { email, senha }
       const dotenv = require("dotenv");
       const jwt = require("jsonwebtoken");
@@ -166,6 +194,41 @@ router
 
   })
 
+  //atualiza o user
+  .put("/updateUser", middlewareValidarJWT, async (req, res) => {
+    try {
+
+      const { email, nome, cargo } = req.body;
+
+      const valUsuario = {
+        nome,
+        email,
+        cargo
+      }
+      
+      const usuarioValidado = validacaoUsuario.parse(valUsuario);
+
+      let resultado = await putUser(
+        usuarioValidado.nome.toLowerCase(),
+        usuarioValidado.email.toLowerCase(),
+        usuarioValidado.cargo.toLowerCase()
+      )
+
+      switch (resultado) {
+        case 200:
+          res.status(200).json('Usuário atualizado')
+          break;
+        case 400:
+          res.status(400).json('Erro ao atualizar usuário')
+          break;
+        default:
+          res.status(500).json('Erro interno do servidor')
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+  })
 
 module.exports = router;
 
