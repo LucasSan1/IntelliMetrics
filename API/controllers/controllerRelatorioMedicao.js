38dfc28a61b25c051d4b2ca6f40d407b9e386950
will <kaiqueprado262@gmail.com>
2024-05-21 17:17:54 -0300

    feat mongo

--- API/controllers/controllerRelatorioMedicao.js
+++ API/controllers/controllerRelatorioMedicao.js
@@ -1,28 +1,17 @@
+const Model = require('../model/model');
 
-const Model = require('../model/model'); // Substitua o caminho pelo caminho real do seu arquivo de modelo
+function infoRelatoirios (lista){
+  
+  const arraySalvar = []
 
-  // Exemplo de como criar e salvar um novo documento sem especificar um ID
-  const novoDocumento = new Model({
-    array: [
-      [1, 2, 3, 5, 7, 8, 9, 10],
-      [1, 2, 3, 5, 7, 8, 9, 10],
-      [1, 2, 3, 5, 7, 8, 9, 10],
-      [1, 2, 3, 5, 7, 8, 9, 10],
-      [1, 2, 3, 5, 7, 8, 9, 10],
-      [1, 2, 3, 5, 7, 8, 9, 10],
-      [1, 2, 3, 5, 7, 8, 9, 10],
-      [1, 2, 3, 5, 7, 8, 9, 10]
-    ]
-  });
+  for(let i = 0; i < 10 ; i++){   
+      arraySalvar.push(lista[i])
 
-  novoDocumento.save()
-    .then(doc => {
-      console.log('Documento inserido com sucesso:', doc);
-    })
-    .catch(err => {
-      console.error('Erro ao inserir documento:', err);
-    });
+  }
+  console.log(arraySalvar)
 
-module.exports ={
-  novoDocumento
 }
+
+module.exports = {
+  infoRelatoirios
+};
