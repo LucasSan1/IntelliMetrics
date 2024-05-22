e668560c678efbc7f8ee6611e03ee3b8658f3f88
will <kaiqueprado262@gmail.com>
2024-05-22 17:26:45 -0300

    feat relatorio

--- API/controllers/controllerRelatorioMedicao.js
+++ API/controllers/controllerRelatorioMedicao.js
@@ -1,15 +1,19 @@
 const Model = require('../model/model');
 
-function infoRelatoirios (lista){
-  
-  const arraySalvar = []
+async function infoRelatoirios(lista) {
+  const documentosSalvos = [];
 
-  for(let i = 0; i < 10 ; i++){   
-      arraySalvar.push(lista[i])
+  for (let i = 0; i < lista.length; i++) {
+    // Crie um novo documento do modelo Relatorio com os dados da lista
+    const novoDocumento = new Model(lista[i]);
 
+    // Salve o documento no banco de dados
+    const documentoSalvo = await novoDocumento.save();
+    documentosSalvos.push(documentoSalvo);
   }
-  console.log(arraySalvar)
 
+  // Retorna os documentos salvos
+  return documentosSalvos;
 }
 
 module.exports = {
